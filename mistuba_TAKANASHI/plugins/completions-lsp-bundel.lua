7c802f81a4597cc71302876ff6f90b2fb48abea7
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-02-24 16:16:57 +0530

    add more completions and snippit, copilet

--- mistuba_TAKANASHI/plugins/completions-lsp-bundel.lua
+++ mistuba_TAKANASHI/plugins/completions-lsp-bundel.lua
@@ -25,6 +25,11 @@ return{
     },
   },
   {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
