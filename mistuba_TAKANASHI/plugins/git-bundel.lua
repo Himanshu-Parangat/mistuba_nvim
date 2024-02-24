dc9667a9ee85f1bfbf7bac77da7da1e1151fc404
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-02-24 16:24:32 +0530

    moved gitsign with fugitive

--- mistuba_TAKANASHI/plugins/git-bundel.lua
+++ mistuba_TAKANASHI/plugins/git-bundel.lua
@@ -1,8 +1,13 @@
-return {
+return{
+	{
+		"tpope/vim-fugitive",
+	},
+{
   "lewis6991/gitsigns.nvim",
   config = function()
     require('gitsigns').setup()
     vim.keymap.set("n", "<Leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", {})
   end
+}
 }
