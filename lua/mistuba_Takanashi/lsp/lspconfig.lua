0319de7a0bb752ece6a2fcd27f4fb70b3e2ba90d
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-07-17 03:53:19 +0530

    reworked lsp setup

--- lua/mistuba_Takanashi/lsp/lspconfig.lua
+++ lua/mistuba_Takanashi/lsp/lspconfig.lua
@@ -1,91 +1,6 @@
--- ensure installed servers 
-local servers = {
-  "lua_ls",
-  -- "pyright",
-  "jedi_language_server",
-}
-
--- mason to handle lsp installation 
-local mason = require("mason")
-mason.setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
-})
-
-local mason_lspconfig = require("mason-lspconfig")
-mason_lspconfig.setup({
-  ensure_installed = servers,
-  automatic_installation = true,
-})
-
--- import lspconfig plugin
-local lspconfig = require("lspconfig")
-
--- import cmp-nvim-lsp plugin
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-
-
-  local client = vim.lsp.get_active_clients({ bufnr = ev.buf })[1]
-    if client then
-      local function lsp_highlight_document(client)
-        if client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            group = "lsp_document_highlight",
-            buffer = ev.buf,
-            callback = function()
-              vim.lsp.buf.document_highlight()
-            end,
-          })
-          vim.api.nvim_create_autocmd("CursorMoved", {
-            group = "lsp_document_highlight",
-            buffer = ev.buf,
-            callback = function()
-              vim.lsp.buf.clear_references()
-            end,
-          })
-        end
-      end
-
-      lsp_highlight_document(client)
-    end
-
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf, silent = true }
-
-
-
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to definition
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- go to implementation
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts) -- go to references
-    vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts) -- go to references
-    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-    vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, opts) -- show diagnostics for line
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-  end,
-})
-
--- used to enable autocompletion (assign to every lsp server config
-local capabilities = cmp_nvim_lsp.default_capabilities()
+-----------------------
+-- diagnostic
+-----------------------
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
@@ -126,77 +41,146 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 })
 
 
+-- diagnostic keymap
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 
+-----------------------
+-- auto cmd for lsp
+-----------------------
 
-mason_lspconfig.setup_handlers({
-  -- default handler for installed servers
-  function(server_name)
-    lspconfig[server_name].setup({
-      capabilities = capabilities,
-    })
-  end,
-  ["lua_ls"] = function()
-    -- configure lua server (with special settings)
-    lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-          completion = {
-            callSnippet = "Replace",
-          },
-        },
-      },
-    })
-  end,
-  ["pyright"] = function()
-    lspconfig["pyright"].setup({
-      capabilities = capabilities,
-        settings = {
-        python = {
-        analysis = {
-          useLibraryCodeForTypes = true,
-          diagnosticSeverityOverrides = {
-            reportGeneralTypeIssues = "none",
-            reportOptionalMemberAccess = "none",
-            reportOptionalSubscript = "none",
-            reportPrivateImportUsage = "none",
-            },
-          }
-        }
-      },
-        -- python = {
-        --   analysis = {
-        --     autoSearchPaths = true,
-        --     diagnosticMode = "workspace",
-        --     useLibraryCodeForTypes = true,
-        --     diagnosticOptions = {
-        --       reportUnknownMemberType = false,
-        --       reportUnusedImport = false,
-        --     },
-        --   },
-        -- },
-        -- diagnosticSeverity = {
-        -- -- Disable the specific diagnostic you want to hide
-        -- ['reportUnusedImport'] = 'none',
-        -- },
-    })
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('mistuba-lsp-attach', { clear = true }),
+  callback = function(event)
+
+    -- keymap shorter
+    local map = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    map('gs', vim.lsp.buf.signature_help, 'signature help')
+
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.server_capabilities.documentHighlightProvider then
+
+      local highlight_augroup = vim.api.nvim_create_augroup('mistuba-lsp-highlight', { clear = false })
+
+      -- cursor rest 
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      -- cursor is up to somting 
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('mistuba-lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'mistuba-lsp-highlight', buffer = event2.buf }
+        end,
+      })
+    end
+
+    -- enable inlay hints if supported
+    if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+      map('<leader>th', function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      end, '[T]oggle Inlay [H]ints')
+    end
   end,
 })
 
+-----------------------
+-- LSP
+-----------------------
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
 
+local servers = {
+
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+            [vim.fn.stdpath("config") .. "/lua"] = true,
+          },
+        },
+        completion = {
+          callSnippet = "Replace",
+        },
+        telemetry = {
+          enable = false
+        },
+        hint = {
+          enable = true
+        }
+      },
+    },
+  },
+}
+
+-----------------------
+-- mason
+-----------------------
+
+-- mason to handle lsp installation 
+local mason = require("mason")
+mason.setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+})
 
 
+local ensure_installed = vim.tbl_keys(servers or {})
+vim.list_extend(ensure_installed, {
+  'stylua', -- Used to format Lua code
+})
+
+local mason_tool_installer = require('mason-tool-installer')
+mason_tool_installer.setup { ensure_installed = ensure_installed }
 
 
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup {
+  handlers = {
+    function(server_name)
+      local server = servers[server_name] or {}
+      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      require('lspconfig')[server_name].setup(server)
+    end,
+  },
+}
--- lua/mistuba_Takanashi/plugins/lsp-mason-bundle.lua
+++ lua/mistuba_Takanashi/plugins/lsp-mason-bundle.lua
@@ -2,4 +2,5 @@ return {
     "neovim/nvim-lspconfig",
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim"
 }
