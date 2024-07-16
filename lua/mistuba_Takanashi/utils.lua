ecc423ae2aaf8d3b969ccd1fe0f0828095045634
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-07-16 20:23:23 +0530

    yank highlight

--- lua/mistuba_Takanashi/utils.lua
+++ lua/mistuba_Takanashi/utils.lua
@@ -147,3 +147,15 @@ cmp.setup {
     native_menu = false,
   },
 }
+
+-----------------------------------------
+-- delay highlight on yank 
+-----------------------------------------
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-my-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({higroup='Visual', timeout=500})
+  end,
+})
