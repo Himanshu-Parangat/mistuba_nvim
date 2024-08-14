ab7e052869f4729b62cf5682f3a8a179f086416f
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-08-14 21:25:18 +0530

    format file

--- lua/mistuba_Takanashi/plugins/colorizer.lua
+++ lua/mistuba_Takanashi/plugins/colorizer.lua
@@ -1,16 +1,15 @@
 return {
-  {
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require 'colorizer'.setup {
-        '*';
-        html = {
-          mode = 'background',
-          names = true;
-        },
-        css = { rgb_fn = true; }, -- Enable parsing rgb(...) functions in css.
-      }
-    end
-  },
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				"*",
+				html = {
+					mode = "background",
+					names = true,
+				},
+				css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
+			})
+		end,
+	},
 }
-
--- lua/mistuba_Takanashi/plugins/indent-blankline.lua
+++ lua/mistuba_Takanashi/plugins/indent-blankline.lua
@@ -1,19 +1,19 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  config = function()
-      require("ibl").setup {
-          indent = {
-            char = "┊"
-          },
-          whitespace = {
-            remove_blankline_trail = true,
-            },
-          scope = {
-            show_start = false,
-            show_end = false
-          }
-      }
-  end,
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	config = function()
+		require("ibl").setup({
+			indent = {
+				char = "┊",
+			},
+			whitespace = {
+				remove_blankline_trail = true,
+			},
+			scope = {
+				show_start = false,
+				show_end = false,
+			},
+		})
+	end,
 }
