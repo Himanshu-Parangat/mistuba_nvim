a797141308935a24a377aea985bef36ebf74e7ff
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-09-03 20:43:04 +0530

    harpoon2 migration

--- lua/mistuba_Takanashi/plugins/harpoon.lua
+++ lua/mistuba_Takanashi/plugins/harpoon.lua
@@ -4,31 +4,59 @@ return {
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local harpoon = require("harpoon")
 
-			-- harpoon
-			-- local harpoon_ui = require("harpoon.ui")
-			-- local harpoon_mark = require("harpoon.mark")
-			-- local opts = { noremap = true, silent = true }
-			-- local keymap = vim.keymap.set
-			--
-			-- keymap("n", "<leader>ht", function()
-			-- 	harpoon_ui.toggle_quick_menu()
-			-- end, opts)
-			-- keymap("n", "<leader>ha", function()
-			-- 	harpoon_mark.add_file()
-			-- end, opts)
-			-- keymap("n", "<leader>hn", function()
-			-- 	harpoon_ui.nav_next()
-			-- end, opts)
-			-- keymap("n", "<leader>hp", function()
-			-- 	harpoon_ui.nav_prev()
-			-- end, opts)
-			--
-			-- for i = 1, 9 do
-			-- 	keymap("n", "<leader>" .. i, function()
-			-- 		harpoon_ui.nav_file(i)
-			-- 	end, opts)
-			-- end
+			-- REQUIRED
+			harpoon:setup()
+			-- REQUIRED
+
+			local keymap = vim.keymap.set
+			keymap("n", "<leader>ha", function()
+				harpoon:list():add()
+			end)
+			keymap("n", "<leader>ht", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			keymap("n", "<leader>hp", function()
+				harpoon:list():prev()
+			end)
+			keymap("n", "<leader>hn", function()
+				harpoon:list():next()
+			end)
+
+			-- harpoon navigation leader followed by 1 to 9
+			for i = 1, 9 do
+				keymap("n", "<leader>" .. i, function()
+					-- harpoon_ui.nav_file(i)
+					harpoon:list():select(i)
+				end)
+			end
+
+			-- basic telescope configuration
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			keymap("n", "<leader>hs", function()
+				toggle_telescope(harpoon:list())
+			end, { desc = "Open harpoon window" })
 		end,
 	},
 }
