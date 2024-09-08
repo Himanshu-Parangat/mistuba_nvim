3fe0dd675d946b2af3eb771fdc416301abeca7fa
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-09-08 11:51:28 +0530

    structure utils

--- lua/mistuba_Takanashi/setup/mappings.lua
+++ lua/mistuba_Takanashi/setup/mappings.lua
@@ -96,106 +96,14 @@ keymap("n", "<leader>cf", ":ToggleTerm direction=float<CR>", "toggle float conso
 -- leave insert_mode
 keymap("t", "<esc>", [[<C-\><C-n>]], "normal mode", opts)
 
--- custome funtion call
-
-function CycleCharacter(step)
-	local line = vim.api.nvim_get_current_line()
-	local col = vim.fn.col(".")
-	local char = string.byte(line:sub(col, col))
-	local new_char
-
-	-- Check if the character is a digit
-	if char >= 48 and char <= 57 then -- ASCII values for digits 0-9
-		-- Increment/decrement the digit
-		new_char = tostring((tonumber(string.char(char)) + step) % 10)
-	elseif char >= 65 and char <= 90 then -- ASCII values for uppercase letters
-		-- Cycle to next/previous uppercase letter
-		new_char = string.char((char - 65 + step) % 26 + 65)
-	elseif char >= 97 and char <= 122 then -- ASCII values for lowercase letters
-		-- Cycle to next/previous lowercase letter
-		new_char = string.char((char - 97 + step) % 26 + 97)
-	else
-		-- Character is neither a digit nor an alphabet, do nothing
-		return
-	end
-
-	vim.api.nvim_set_current_line(line:sub(1, col - 1) .. new_char .. line:sub(col + 1))
-end
-
--- Map the function to the keybindings
-keymap("n", "<leader>+", ":lua CycleCharacter(1)<CR>", "Increment character", opts)
-keymap("n", "<leader>-", ":lua CycleCharacter(-1)<CR>", "Decrement character", opts)
-
--- Toggle virtual text display for the current line
-function Supress_virtual_line_text()
-	local current_line = vim.fn.line(".")
-	local bufnr = vim.api.nvim_get_current_buf()
-	vim.api.nvim_buf_clear_namespace(bufnr, 0, current_line - 1, current_line)
-end
-
-keymap("n", "<Leader>tl", ":lua Supress_virtual_line_text()<CR>", "toggle(supress) line vtext", opts)
-
--- Function to toggle diagnostics
-local diagnostics_active = true
-function Toggle_diagnostics()
-	diagnostics_active = not diagnostics_active
-	if diagnostics_active then
-		vim.diagnostic.show()
-	else
-		vim.diagnostic.hide()
-	end
-end
-
-keymap("n", "<leader>td", ":lua Toggle_diagnostics()<CR>", "toggle diagnostics", opts)
-
--- Function to toggle virtual text
-_G.vt_state = true
-
-function Toggle_virtual_text()
-	_G.vt_state = not _G.vt_state
-
-	vim.diagnostic.config({
-		virtual_text = _G.vt_state,
-	})
-
-	local status = _G.vt_state and "enabled" or "disabled"
-	print("virtual text " .. status)
-end
-
--- Function to toggle diagnostic signs
-_G.sign_state = true
-
-function Toggle_signs()
-	_G.sign_state = not _G.sign_state
-
-	vim.diagnostic.config({
-		signs = _G.sign_state,
-	})
-
-	local status = _G.sign_state and "enabled" or "disabled"
-	print("diagnostic signs " .. status)
-end
-
--- Function to toggle diagnostic underline
-_G.underline_state = true
-
-function Toggle_underline()
-	_G.underline_state = not _G.underline_state
-
-	vim.diagnostic.config({
-		underline = _G.underline_state,
-	})
-
-	local status = _G.underline_state and "enabled" or "disabled"
-	print("diagnostic underline " .. status)
-end
-
-keymap("n", "<leader>tv", ":lua Toggle_virtual_text()<CR>", "toggle virtual text", opts)
-keymap("n", "<leader>ts", ":lua Toggle_signs()<CR>", "toggle diagnostic signs", opts)
-keymap("n", "<leader>tu", ":lua Toggle_underline()<CR>", "toggle diagnostic underline", opts)
-
-
 local utils = require("mistuba_Takanashi.setup.utils")
 
+vim.keymap.set("n", "<leader>+", function() utils.cycleCharacter(1) end, { desc = "Toggle hidden characters", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>-", function() utils.cycleCharacter(-1) end, { desc = "Toggle hidden characters", noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>tc", utils.toggleListChars, { desc = "Toggle hidden characters", noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>tl", utils.supress_virtual_line_text, { desc = "Toggle hidden characters", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>td", utils.toggle_diagnostics, { desc = "Toggle diagnostic", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tv", utils.toggle_virtual_text, { desc = "Toggle virtual text", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ts", utils.toggle_signs, { desc = "Toggle diagnostic signs", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tu", utils.toggle_underline, { desc = "Toggle diagnostic underline", noremap = true, silent = true })
--- lua/mistuba_Takanashi/setup/utils.lua
+++ lua/mistuba_Takanashi/setup/utils.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -------------------------------------------
--- resore cursor position
+-- restore cursor position
 -------------------------------------------
 
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
@@ -45,5 +45,113 @@ function utils.toggleListChars()
 	end
 end
 
+
+---------------------------------------------------
+-- Toggle virtual text display for the current line
+---------------------------------------------------
+
+function utils.supress_virtual_line_text()
+	local current_line = vim.fn.line(".")
+	local bufnr = vim.api.nvim_get_current_buf()
+	vim.api.nvim_buf_clear_namespace(bufnr, 0, current_line - 1, current_line)
+end
+
+
+---------------------------------
+-- Function to toggle diagnostics
+---------------------------------
+
+local diagnostics_active = true
+function utils.toggle_diagnostics()
+	diagnostics_active = not diagnostics_active
+	if diagnostics_active then
+		vim.diagnostic.show()
+	else
+		vim.diagnostic.hide()
+	end
+end
+
+
+----------------------------------
+-- Function to toggle virtual text
+----------------------------------
+
+_G.vt_state = true
+
+function utils.toggle_virtual_text()
+	_G.vt_state = not _G.vt_state
+
+	vim.diagnostic.config({
+		virtual_text = _G.vt_state,
+	})
+
+	local status = _G.vt_state and "enabled" or "disabled"
+	print("virtual text " .. status)
+end
+
+--------------------------------------
+-- Function to toggle diagnostic signs
+--------------------------------------
+
+_G.sign_state = true
+
+function utils.toggle_signs()
+	_G.sign_state = not _G.sign_state
+
+	vim.diagnostic.config({
+		signs = _G.sign_state,
+	})
+
+	local status = _G.sign_state and "enabled" or "disabled"
+	print("diagnostic signs " .. status)
+end
+
+
+------------------------------------------
+-- Function to toggle diagnostic underline
+------------------------------------------
+
+_G.underline_state = true
+
+function utils.toggle_underline()
+	_G.underline_state = not _G.underline_state
+
+	vim.diagnostic.config({
+		underline = _G.underline_state,
+	})
+
+	local status = _G.underline_state and "enabled" or "disabled"
+	print("diagnostic underline " .. status)
+end
+
+
+-----------------------
+-- custome funtion call
+-----------------------
+
+
+function utils.cycleCharacter(step)
+	local line = vim.api.nvim_get_current_line()
+	local col = vim.fn.col(".")
+	local char = string.byte(line:sub(col, col))
+	local new_char
+
+	-- Check if the character is a digit
+	if char >= 48 and char <= 57 then -- ASCII values for digits 0-9
+		-- Increment/decrement the digit
+		new_char = tostring((tonumber(string.char(char)) + step) % 10)
+	elseif char >= 65 and char <= 90 then -- ASCII values for uppercase letters
+		-- Cycle to next/previous uppercase letter
+		new_char = string.char((char - 65 + step) % 26 + 65)
+	elseif char >= 97 and char <= 122 then -- ASCII values for lowercase letters
+		-- Cycle to next/previous lowercase letter
+		new_char = string.char((char - 97 + step) % 26 + 97)
+	else
+		-- Character is neither a digit nor an alphabet, do nothing
+		return
+	end
+
+	vim.api.nvim_set_current_line(line:sub(1, col - 1) .. new_char .. line:sub(col + 1))
+end
 
 return utils
