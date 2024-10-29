6faa2ce6ecb57e8dd2f4cb4bd5871455cc0e683d
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-10-29 22:14:24 +0530

    completion rework

--- lua/mistuba_Takanashi/completion/completion.lua
+++ lua/mistuba_Takanashi/completion/completion.lua
@@ -8,78 +8,26 @@ if not cmp_status_ok then
 	return
 end
 
-local snip_status_ok, luasnip = pcall(require, "luasnip")
-if not snip_status_ok then
-	return
-end
-
-require("luasnip/loaders/from_vscode").lazy_load()
-
-local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 --   פּ ﯟ   some other good icons
 local kind_icons = require("mistuba_Takanashi.icons.default")
 
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
-	mapping = {
-		["<Up>"] = cmp.mapping.select_prev_item(),
-		["<Down>"] = cmp.mapping.select_next_item(),
-		["<C-Up>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		["<C-Down>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<Esc>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		-- ["<C-k>"] = cmp.mapping.select_prev_item(),
-		-- ["<C-j>"] = cmp.mapping.select_next_item(),
-		-- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		-- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		-- ["<Esc>"] = cmp.mapping {
-		--   i = cmp.mapping.abort(),
-		--   c = cmp.mapping.close(),
-		-- },
-		-- Accept currently selected item. If none selected, `select` first item.
-		-- Set `select` to `false` to only confirm explicitly selected items.
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif check_backspace() then
-				fallback()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
@@ -87,34 +35,90 @@ cmp.setup({
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				nvim_lua = "[NVIM_LUA]",
 				luasnip = "[Snippet]",
 				buffer = "[Buffer]",
-				path = "[Path]",
-				emoji = "[Emoji]",
+				calc = "[calc]",
+				look = "[look]",
 			})[entry.source.name]
 			return vim_item
 		end,
 	}, --
-	sources = {
+	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "luasnip" },
+		{ name = "luasnip" }, -- For luasnip users.
+		{ name = "calc" },
+	}, {
+		-- completion from All buffers
+		{
+			name = "buffer",
+			option = {
+				keyword_length = 3,
+				get_bufnrs = function()
+					return vim.api.nvim_list_bufs()
+				end,
+			},
+		},
+		-- sudo pacman -S words
+		{
+			name = "look",
+			keyword_length = 2,
+			option = {
+				convert_case = true,
+				loud = true,
+				dict = {
+					"/usr/share/dict/words",
+				},
+			},
+		},
+		{
+			-- get_cwd
+			name = "path",
+			option = {
+				-- Options go into this table
+				trailing_slash = false,
+			},
+		},
+	}),
+})
+
+-- `/` cmdline setup.
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
 		{ name = "buffer" },
-		{ name = "path" },
-		{ name = "emoji" },
-		-- { name = "cmdline" },
-	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
-	},
-	window = {
-		documentation = cmp.config.window.bordered(),
-	},
-	experimental = {
-		ghost_text = true,
-		native_menu = false,
 	},
 })
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{
+			name = "cmdline",
+			option = {
+				ignore_cmds = { "Man", "!" },
+			},
+		},
+	}),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline({ "/", "?" }, {
+-- 	mapping = cmp.mapping.preset.cmdline(),
+-- 	sources = {
+-- 		{ name = "buffer" },
+-- 	},
+-- })
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(":", {
+-- 	mapping = cmp.mapping.preset.cmdline(),
+-- 	sources = cmp.config.sources({
+-- 		{ name = "path" },
+-- 	}, {
+-- 		{ name = "cmdline" },
+-- 	}),
+-- 	matching = { disallow_symbol_nonprefix_matching = false },
+-- })
--- lua/mistuba_Takanashi/plugins/cmp.lua
+++ lua/mistuba_Takanashi/plugins/cmp.lua
@@ -1,16 +1,19 @@
 return {
-  -- snippets
-   "rafamadriz/friendly-snippets", -- a bunch of snippets to 
-   "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-   "nvim-lua/plenary.nvim",
-   "L3MON4D3/LuaSnip", --snippet engine
--- cmp plugins
-   "hrsh7th/nvim-cmp", -- The completion plugin
-   "hrsh7th/cmp-buffer", -- buffer completions
-   "hrsh7th/cmp-path", -- path completions
-   "hrsh7th/cmp-cmdline", -- cmdline completions
-   "hrsh7th/cmp-nvim-lsp", -- lsp completions
-   "saadparwaiz1/cmp_luasnip", -- snippet completions
-   "hrsh7th/cmp-emoji",
-   "chrisgrieser/cmp-nerdfont"
+	"hrsh7th/nvim-cmp", -- The completion plugin
+	"L3MON4D3/LuaSnip", --snippet engine
+	"hrsh7th/cmp-calc", -- nvim-cmp source for math calculation
+	"hrsh7th/cmp-buffer", -- nvim-cmp source for buffer words
+	"octaltree/cmp-look",
+
+	-- snippets
+	"rafamadriz/friendly-snippets", -- a bunch of snippets to
+	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+	"nvim-lua/plenary.nvim",
+	-- cmp plugins
+	"hrsh7th/cmp-path", -- path completions
+	"hrsh7th/cmp-cmdline", -- cmdline completions
+	"hrsh7th/cmp-nvim-lsp", -- lsp completions
+	"saadparwaiz1/cmp_luasnip", -- snippet completions
+	"hrsh7th/cmp-emoji",
+	"chrisgrieser/cmp-nerdfont",
 }
